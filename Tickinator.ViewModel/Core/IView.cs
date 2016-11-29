ebfa920639bd1712763f4d57846ec5f62edd5d28
Ed Mays <copperstarsystems@gmail.com>
2016-11-29 14:51:58 -0500

    Introduce TicketDetailsView

---
+++ Tickinator.ViewModel/Core/IView.cs
@@ -0,0 +1,14 @@
+﻿//  --------------------------------------------------------------------------------------
+// Tickinator.ViewModel.IView.cs
+// 2016/11/29
+//  --------------------------------------------------------------------------------------
+
+namespace Tickinator.ViewModel.Core
+{
+    public interface IView
+    {
+        object DataContext { get; set; }
+
+        void Show();
+    }
+}
---
+++ Tickinator.ViewModel/TicketList/ITicketDetailsView.cs
@@ -0,0 +1,13 @@
+﻿//  --------------------------------------------------------------------------------------
+// Tickinator.ViewModel.ITicketDetailsView.cs
+// 2016/11/29
+//  --------------------------------------------------------------------------------------
+
+using Tickinator.ViewModel.Core;
+
+namespace Tickinator.ViewModel.TicketList
+{
+    public interface ITicketDetailsView : IView
+    {
+    }
+}
