a0240a5f4b83eaf00571f6a7f8acacc0827a3bf2
Ed Mays <copperstarsystems@gmail.com>
2016-12-02 15:56:38 -0500

    Issue 2:  ShowTicketDetailsCommand Implementation

--- Tickinator.ViewModel.Tests/Command/ShowTicketDetailsCommandTests.cs
+++ Tickinator.ViewModel.Tests/Command/ShowTicketDetailsCommandTests.cs
@@ -3,10 +3,14 @@
 // 2016/11/29
 //  --------------------------------------------------------------------------------------
 
+using System.Collections.Generic;
 using Moq;
 using NUnit.Framework;
+using Tickinator.Model;
+using Tickinator.Repository;
 using Tickinator.ViewModel.Command;
 using Tickinator.ViewModel.Tests.Command.Core;
+using Tickinator.ViewModel.TicketDetails;
 using Tickinator.ViewModel.TicketList;
 using Tickinator.ViewModel.View;
 using Tickinator.ViewModel.View.Core;
@@ -16,16 +20,22 @@ namespace Tickinator.ViewModel.Tests.Command
     [TestFixture]
     public class ShowTicketDetailsCommandTests : CommandBaseTests<ShowTicketDetailsCommand>
     {
+        Mock<ICloseCommand> mockCloseCommand;
+        Mock<ICloseCommandFactory> mockCloseCommandFactory;
         Mock<ITicketDetailsView> mockTicketDetailsView;
+        Mock<ITicketDetailsViewModel> mockTicketDetailsViewModel;
+        Mock<ITicketDetailsViewModelFactory> mockTicketDetailsViewModelFactory;
+        Mock<ITicketRepository> mockTicketRepository;
         Mock<IViewFactory> mockViewFactory;
         Mock<ITicketListItemViewModel> mockViewModel;
+        IList<Ticket> tickets;
 
-        [Test]
-        public void Execute_Always_PerformsExpectedWork()
+        [TestCase(1)]
+        [TestCase(3)]
+        public void Execute_Always_PerformsExpectedWork(int ticketId)
         {
-            mockViewFactory.Setup(p => p.Create<ITicketDetailsView>()).Returns(mockTicketDetailsView.Object);
-            mockTicketDetailsView.SetupSet(p => p.DataContext = mockViewModel.Object);
-            mockTicketDetailsView.Setup(p => p.ShowDialog()).Returns(true);
+            CreateTickets();
+            SetupMocksForExecuteTest(ticketId);
             Execute(mockViewModel.Object);
             MockRepository.VerifyAll();
         }
@@ -36,11 +46,38 @@ namespace Tickinator.ViewModel.Tests.Command
             mockViewFactory = CreateMock<IViewFactory>();
             mockTicketDetailsView = CreateMock<ITicketDetailsView>();
             mockViewModel = CreateMock<ITicketListItemViewModel>();
+            mockTicketDetailsViewModelFactory = CreateMock<ITicketDetailsViewModelFactory>();
+            mockTicketRepository = CreateMock<ITicketRepository>();
+            mockCloseCommandFactory = CreateMock<ICloseCommandFactory>();
+            mockCloseCommand = CreateMock<ICloseCommand>();
+            mockTicketDetailsViewModel = CreateMock<ITicketDetailsViewModel>();
         }
 
         protected override ShowTicketDetailsCommand CreateSystemUnderTest()
         {
-            return new ShowTicketDetailsCommand(mockViewFactory.Object);
+            return new ShowTicketDetailsCommand(mockViewFactory.Object, mockTicketDetailsViewModelFactory.Object,
+                mockTicketRepository.Object, mockCloseCommandFactory.Object);
+        }
+
+        void CreateTickets()
+        {
+            tickets = new List<Ticket>();
+
+            for (var i = 0; i < 5; i++)
+                tickets.Add(new Ticket {Id = i + 1});
+        }
+
+        void SetupMocksForExecuteTest(int ticketId)
+        {
+            mockViewFactory.Setup(p => p.Create<ITicketDetailsView>()).Returns(mockTicketDetailsView.Object);
+            mockTicketRepository.Setup(p => p.GetAll()).Returns(tickets);
+            mockViewModel.SetupGet(p => p.Id).Returns(ticketId);
+            mockTicketDetailsViewModelFactory.Setup(p => p.Create(tickets[ticketId - 1], mockCloseCommand.Object))
+                .Returns(mockTicketDetailsViewModel.Object);
+            mockTicketDetailsView.SetupSet(p => p.DataContext = mockTicketDetailsViewModel.Object);
+            mockCloseCommandFactory.Setup(p => p.Create(mockTicketDetailsView.Object)).Returns(mockCloseCommand.Object);
+
+            mockTicketDetailsView.Setup(p => p.ShowDialog()).Returns(true);
         }
     }
 }
--- Tickinator.ViewModel/Command/ShowTicketDetailsCommand.cs
+++ Tickinator.ViewModel/Command/ShowTicketDetailsCommand.cs
@@ -3,7 +3,10 @@
 // 2016/11/29
 //  --------------------------------------------------------------------------------------
 
+using System.Linq;
+using Tickinator.Repository;
 using Tickinator.ViewModel.Command.Core;
+using Tickinator.ViewModel.TicketDetails;
 using Tickinator.ViewModel.TicketList;
 using Tickinator.ViewModel.View;
 using Tickinator.ViewModel.View.Core;
@@ -12,18 +15,41 @@ namespace Tickinator.ViewModel.Command
 {
     public class ShowTicketDetailsCommand : GenericCommandBase<ITicketListItemViewModel>, IShowTicketDetailsCommand
     {
+        readonly ICloseCommandFactory closeCommandFactory;
+        readonly ITicketDetailsViewModelFactory ticketDetailsViewModelFactory;
+        readonly ITicketRepository ticketRepository;
         readonly IViewFactory viewFactory;
 
-        public ShowTicketDetailsCommand(IViewFactory viewFactory)
+        public ShowTicketDetailsCommand(IViewFactory viewFactory,
+            ITicketDetailsViewModelFactory ticketDetailsViewModelFactory, ITicketRepository ticketRepository,
+            ICloseCommandFactory closeCommandFactory)
         {
             this.viewFactory = viewFactory;
+            this.ticketDetailsViewModelFactory = ticketDetailsViewModelFactory;
+            this.ticketRepository = ticketRepository;
+            this.closeCommandFactory = closeCommandFactory;
         }
 
         protected override void ExecuteInternal(ITicketListItemViewModel parameter)
         {
-            var view = viewFactory.Create<ITicketDetailsView>();
-            view.DataContext = parameter;
+            var view = CreateView();
+            var viewModel = CreateViewModel(parameter, view);
+            view.DataContext = viewModel;
             view.ShowDialog();
+        }
+
+        ITicketDetailsView CreateView()
+        {
+            var view = viewFactory.Create<ITicketDetailsView>();
+            return view;
+        }
+
+        ITicketDetailsViewModel CreateViewModel(ITicketListItemViewModel parameter, ITicketDetailsView view)
+        {
+            var closeCommand = closeCommandFactory.Create(view);
+            var ticket = ticketRepository.GetAll().FirstOrDefault(p => p.Id == parameter.Id);
+            var viewModel = ticketDetailsViewModelFactory.Create(ticket, closeCommand);
+            return viewModel;
         }
     }
 }
