bf13fd8c037451d7f414c5a59fd56ceadaf55dca
Ed Mays <copperstarsystems@gmail.com>
2016-12-06 14:35:01 -0500

    Issue 3:  Finish out TechnicianListProvider

--- Tickinator.ViewModel.Tests/TechnicianList/TechnicianListProviderTests.cs
+++ Tickinator.ViewModel.Tests/TechnicianList/TechnicianListProviderTests.cs
@@ -3,7 +3,12 @@
 // 2016/12/06
 //  --------------------------------------------------------------------------------------
 
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
 using NUnit.Framework;
+using Tickinator.Model;
+using Tickinator.Repository;
 using Tickinator.ViewModel.TechnicianList;
 
 namespace Tickinator.ViewModel.Tests.TechnicianList
@@ -11,18 +16,42 @@ namespace Tickinator.ViewModel.Tests.TechnicianList
     [TestFixture]
     public class TechnicianListProviderTests : TestBase<TechnicianListProvider>
     {
-
-        // Stub
-        [Test]
-        public void GetTechnicians_Always_ReturnsExpectedValue()
+        Mock<ITechnicianRepository> mockTechnicianRepository;
+        Mock<ITechnicianListItemViewModelFactory> mockListItemViewModelFactory;
+        List<Technician> technicians;
+        protected override void CreateMocks()
         {
-            Assert.That(SystemUnderTest.GetTechnicians(), Is.Not.Null);
-            Assert.That(SystemUnderTest.GetTechnicians(), Is.Empty);
+            base.CreateMocks();
+            technicians=new List<Technician>();
+            mockTechnicianRepository = CreateMock<ITechnicianRepository>();
+            mockListItemViewModelFactory = CreateMock<ITechnicianListItemViewModelFactory>();
+        }
+
+        protected override void SetupConstructorRequiredMocks()
+        {
+            base.SetupConstructorRequiredMocks();
+            mockTechnicianRepository.Setup(p => p.GetAll()).Returns(technicians);
         }
 
         protected override TechnicianListProvider CreateSystemUnderTest()
         {
-            return new TechnicianListProvider();
+            return new TechnicianListProvider(mockTechnicianRepository.Object, mockListItemViewModelFactory.Object);
+        }
+
+        [TestCase(3)]
+        [TestCase(10)]
+        public void GetTechnicians_Always_ReturnsExpectedValue(int technicianCount)
+        {
+            for (int i = 0; i < technicianCount; i++)
+            {
+                var technician = new Technician();
+                technicians.Add(technician);
+                var listItemViewModel = CreateMock<ITechnicianListItemViewModel>();
+                mockListItemViewModelFactory.Setup(p => p.Create(technician)).Returns(listItemViewModel.Object);
+            }
+            RecreateSystemUnderTest();
+            Assert.That(SystemUnderTest.GetTechnicians(), Is.Not.Null);
+            Assert.That(SystemUnderTest.GetTechnicians().Count(), Is.EqualTo(technicianCount));
         }
     }
 }
--- Tickinator.ViewModel/TechnicianList/TechnicianListProvider.cs
+++ Tickinator.ViewModel/TechnicianList/TechnicianListProvider.cs
@@ -3,17 +3,36 @@
 // 2016/12/06
 //  --------------------------------------------------------------------------------------
 
-using System;
 using System.Collections.Generic;
+using Tickinator.Model;
+using Tickinator.Repository;
 
 namespace Tickinator.ViewModel.TechnicianList
 {
     public class TechnicianListProvider : ITechnicianListProvider
     {
-        // Stub
+        readonly ITechnicianRepository technicianRepository;
+        IList<ITechnicianListItemViewModel> technicians;
+
+        public TechnicianListProvider(ITechnicianRepository technicianRepository,
+            ITechnicianListItemViewModelFactory listItemViewModelFactory)
+        {
+            CreateList(technicianRepository.GetAll(), listItemViewModelFactory);
+        }
+
         public IEnumerable<ITechnicianListItemViewModel> GetTechnicians()
         {
-            return new List<ITechnicianListItemViewModel>();
+            return technicians;
+        }
+
+        void CreateList(IEnumerable<Technician> technicianEntities, ITechnicianListItemViewModelFactory factory)
+        {
+            technicians = new List<ITechnicianListItemViewModel>();
+            foreach (var technicianEntity in technicianEntities)
+            {
+                var technician = factory.Create(technicianEntity);
+                technicians.Add(technician);
+            }
         }
     }
 }
