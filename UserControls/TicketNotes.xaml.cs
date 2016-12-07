12b715deced28ed4fce4e86fe1e130584b4b4b52
Ed Mays <copperstarsystems@gmail.com>
2016-12-07 09:00:15 -0500

    Introduce TicketNotes.xaml

---
+++ UserControls/TicketNotes.xaml.cs
@@ -0,0 +1,17 @@
+﻿//  --------------------------------------------------------------------------------------
+// Tickinator.UI.Wpf.TicketNotes.xaml.cs
+// 2016/12/06
+//  --------------------------------------------------------------------------------------
+
+using System.Windows.Controls;
+
+namespace Tickinator.UI.Wpf.UserControls
+{
+    public partial class TicketNotes : UserControl
+    {
+        public TicketNotes()
+        {
+            InitializeComponent();
+        }
+    }
+}
