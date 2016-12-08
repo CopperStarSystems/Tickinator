94abeddd120b902966260ffc92d89c3c90151329
Ed Mays <copperstarsystems@gmail.com>
2016-12-08 14:22:46 -0500

    Extract strings from TicketDetailsView

--- Tickinator.Common/Strings.cs
+++ Tickinator.Common/Strings.cs
@@ -7,13 +7,19 @@ namespace Tickinator.Common
 {
     public class Strings
     {
+        public class Button
+        {
+            public const string Cancel = "_Cancel";
+            public const string Save = "_Save";
+        }
+
         public class Dashboard
         {
+            public const string AverageTicketDuration = "Average Ticket Duration:";
+            public const string CurrentOpenTickets = "Current Open Tickets:";
             public const string MyDashboardTitle = "My Dashboard:";
             public const string TeamDashboardTitle = "Team Dashboard";
-            public const string CurrentOpenTickets = "Current Open Tickets:";
             public const string TicketsClosedToday = "Tickets Closed Today:";
-            public const string AverageTicketDuration = "Average Ticket Duration:";
         }
 
         public class MainWindow
@@ -24,7 +30,14 @@ namespace Tickinator.Common
         public class TicketDetails
         {
             public const string AddHeaderText = "New Ticket:";
+            public const string AssignedToLabel = "Assigned To:";
+            public const string DateLabel = "Date:";
             public const string EditHeaderText = "Ticket Details:";
+            public const string HistoryTabHeader = "History";
+            public const string NotesTabHeader = "Notes";
+            public const string StatusLabel = "Status:";
+            public const string SummaryTabHeader = "Summary";
+            public const string TicketNumberLabel = "Ticket Number:";
         }
     }
 }
